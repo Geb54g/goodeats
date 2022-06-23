07a3053c8570931ac8eca71d1266a8f7954799ae
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-06-24 01:15:35 +0300

    st:js form validation

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('#create-account-form');
+const usernameInput = document.querySelector('#username');
+const emailIput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+const confirmPasswordInput = document.querySelector('#confirm-password');
